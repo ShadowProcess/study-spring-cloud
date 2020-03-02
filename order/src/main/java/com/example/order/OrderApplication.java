4c9786e4276dd17435d9a4fe879e18e32610d886
alex <jiahang@liangteam.com>
2020-03-02 17:01:22 +0800

    收尾

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -7,13 +7,15 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableDiscoveryClient //发现Eureka服务
+
 @EnableFeignClients(basePackages = "com.example.order.client")    //feign:客户端负载均衡器
 @EnableCircuitBreaker //circuit[电路] breaker[断路器]
-
+@SpringBootApplication
+@EnableDiscoveryClient //发现Eureka服务
 //@SpringCloudApplication //里边包含cloud需要的基本注解，包含上面三个
+@ComponentScan(basePackages = "com.example") //配置扫描范围
 public class OrderApplication {
 
     public static void main(String[] args) {
--- order/src/main/java/com/example/order/client/ProductClient.java
+++ order/src/main/java/com/example/order/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.example.order.client;
 import com.example.order.dataobject.ProductInfo;
 import com.example.order.dto.CartDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ import java.util.List;
  * 用于调用另外一个服务[产品服务]
  */
 
-@FeignClient(name = "product") //参数name,表示你要访问那个应用的接口
+@FeignClient(name = "product",fallback = ProductClient.ProductClientFallback.class) //参数name,表示你要访问那个应用的接口
 public interface ProductClient {
 
     @GetMapping("/msg")
@@ -28,4 +29,26 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<CartDto> cartDtoList);
+
+
+    @Component
+    static class ProductClientFallback implements ProductClient {
+        @Override
+        public String productMsg() {
+            return null;
+        }
+        /**
+         * 如果触发服务降级，那么会调用这个方法
+         * @param productIdList
+         * @return
+         */
+        @Override
+        public List<ProductInfo> listForOrder(List<String> productIdList) {
+            return null;
+        }
+        @Override
+        public void decreaseStock(List<CartDto> cartDtoList) {
+
+        }
+    }
 }
