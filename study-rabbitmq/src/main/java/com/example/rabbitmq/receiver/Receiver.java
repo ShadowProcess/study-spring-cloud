c4d92de00358c7c2f1133620ae01bb51a5076ddf
alex <jiahang@liangteam.com>
2020-04-08 18:07:08 +0800

    rabbitMQ 持久化消息,持久化队列,持久化路由

--- study-rabbitmq/src/main/java/com/example/rabbitmq/receiver/Receiver.java
+++ study-rabbitmq/src/main/java/com/example/rabbitmq/receiver/Receiver.java
@@ -7,18 +7,32 @@ import org.springframework.stereotype.Component;
  * @author Alex
  *
  * 演示RabbitMQ接收端
+ *
+ *
+ * *       消息接收者
+ * *
+ * *         1、@RabbitListener bindings:绑定队列
+ * *
+ * *         2、@QueueBinding
+ * *         value:绑定队列的名称、exchange:配置交换器、key:路由键routing-key绑定队列和交换器
+ * *
+ * *         3、@Queue value:配置队列名称、autoDelete:是否是一个可删除的临时队列
+ * *
+ * *         4、@Exchange value:为交换器起个名称、type:指定具体的交换器类型
+ *
  */
 @Component
 public class Receiver {
 
     /**
      * 电脑类接收消息
+     * autoDelete的意思是：如果为true的话，那么没有消费者订阅的队列，会被自动删除。
      * @param hello
      */
     @RabbitListener(bindings = @QueueBinding(
-            exchange = @Exchange("myElectronics"),
+            exchange = @Exchange(value = "myElectronics"),  //交换机默认就是持久化
             key = "computer",
-            value = @Queue("computerElectronics")
+            value = @Queue(value = "computerElectronics",durable = "true")   //声明队列持久化
     ))
     public void processComputer(String hello) {
         System.out.println("C-Receiver:" + hello);
@@ -26,12 +40,13 @@ public class Receiver {
 
     /**
      * 手机类接收消息
+     * autoDelete的意思是：如果为true的话，那么没有消费者订阅的队列，会被自动删除。
      * @param hello
      */
     @RabbitListener(bindings = @QueueBinding(
-            exchange = @Exchange("myElectronics"),
+            exchange = @Exchange("myElectronics"),      //交换机默认就是持久化的
             key = "phone",
-            value = @Queue("phoneElectronics")
+            value = @Queue(value = "phoneElectronics",durable = "true")    //声明队列持久化
     ))
     public void processPhone(String hello) {
         System.out.println("P-Receiver:" + hello);
--- study-rabbitmq/src/main/java/com/example/rabbitmq/sender/Sender.java
+++ study-rabbitmq/src/main/java/com/example/rabbitmq/sender/Sender.java
@@ -2,7 +2,11 @@ package com.example.rabbitmq.sender;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
@@ -13,7 +17,7 @@ import java.util.Date;
 
 /**
  * @author Alex
- *
+ * <p>
  * 演示RabbitMQ发送端
  */
 
@@ -34,6 +38,12 @@ public class Sender {
         }
     }
 
+    /**
+     * RabbitMQ消息持久化
+     * 使用convertAndSend方式发送消息，消息默认就是持久化的.
+     * new MessageProperties() --> DEFAULT_DELIVERY_MODE = MessageDeliveryMode.PERSISTENT --> deliveryMode = 2;
+     */
+
 
     @Test
     public void sendPhone() {
@@ -41,5 +51,18 @@ public class Sender {
             String content = "发送信息到手机队列，当前时间：" + new Date();
             this.rabbitmqTemplate.convertAndSend("myElectronics", "phone", content);
         }
+    }
+
+
+    //发送持久化消息
+    public void sendMessage(final String exchange, final String routingKey, final String content)
+            throws AmqpException {
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setContentType(MessageProperties.CONTENT_TYPE_BYTES);
+        messageProperties.setDeliveryMode(MessageProperties.DEFAULT_DELIVERY_MODE);
+        Message message = new Message(content.getBytes(), messageProperties);
+
+        this.rabbitmqTemplate.send(exchange, routingKey, message);
     }
 }
